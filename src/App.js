0759795752378edde4023e5160946ab618903deb
Tajveez <tajveez786@gmail.com>
2020-07-12 20:23:04 +0500

    React Router adde

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React, { Component } from "react";
-
 import "./App.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import Todos from "./components/Todos";
 import AddTodo from "./components/AddTodo";
+import About from "./components/pages/About";
+
 import { v4 as uuid } from "uuid";
 
 import Header from "./components/layout/Header";
@@ -54,17 +56,28 @@ class App extends Component {
   render() {
     // console.log();
     return (
-      <div className="App">
-        <div className="container">
-          <Header />
-          <AddTodo addTodo={this.addTodo} />
-          <Todos
-            todos={this.state.todos}
-            markComplete={this.markComplete}
-            deleteTodo={this.deleteTodo}
-          />
+      <Router>
+        <div className="App">
+          <div className="container">
+            <Header />
+            <Route
+              exact
+              path="/"
+              render={(props) => (
+                <React.Fragment>
+                  <AddTodo addTodo={this.addTodo} />
+                  <Todos
+                    todos={this.state.todos}
+                    markComplete={this.markComplete}
+                    deleteTodo={this.deleteTodo}
+                  />
+                </React.Fragment>
+              )}
+            />
+            <Route path="/about" component={About} />
+          </div>
         </div>
-      </div>
+      </Router>
     );
   }
 }
--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,14 +1,25 @@
 import React from "react";
+import { Link } from "react-router-dom";
 const headerStyle = {
   background: "#333",
   color: "white",
   textAlign: "center",
   padding: "10px",
 };
+const linkStyle = {
+  color: "white",
+};
 export default function Header() {
   return (
-    <div>
-      <h1 style={headerStyle}>TodoList</h1>
+    <div style={headerStyle}>
+      <h1>TodoList</h1>
+      <Link style={linkStyle} to="/">
+        Home
+      </Link>{" "}
+      |{" "}
+      <Link style={linkStyle} to="/about">
+        About
+      </Link>
     </div>
   );
 }
